1fefa606d5dcac3b9b4a387a24c21f4f7619735d
Pablo Salgado <pablo.salgado@iungoeducation.com>
2017-11-25 07:15:02 -0500

    Add CRUD operations

---
+++ CrearTabla.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "bdunad24";
+
+// Establecer conexión al servidor MySQL local
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Abortar si no se puede establecer conexión al servidor
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+
+// sql para crear la taba
+$sql = "CREATE TABLE IF NOT EXISTS tabla24 (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombres VARCHAR(30) NOT NULL,
+  apellidos VARCHAR(30) NOT NULL,
+  registro INT UNSIGNED NOT NULL,
+  identificacion INT UNSIGNED NOT NULL,
+  telefono VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  direccion VARCHAR(50),
+  especialidad VARCHAR(50),
+  hospital VARCHAR(50)
+)";
+
+if ($conn->query($sql) === TRUE) {
+   echo "Tabla creada éxitosamente";
+} else {
+   echo "Ocurrió un error al crear la tabla: " . $conn->error;
+}
+
+$conn->close();
+?>
--- src/AppBundle/Controller/IngresoController.php
+++ src/AppBundle/Controller/IngresoController.php
@@ -7,30 +7,95 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use \mysqli;
 
 class IngresoController extends Controller
 {
+
     /**
      * @Route("/ingreso")
      */
     public function ingresoForm(Request $request)
     {
+        $servername = "localhost";
+        $username = "root";
+        $password = "12345678";
+        $dbname = "bdunad24";
+
         $post = Request::createFromGlobals();
 
+        $nombres = $post->request->get('nombres');
+        $apellidos = $post->request->get('apellidos');
+        $registro = $post->request->get('registro');
+        $identificacion = $post->request->get('identificacion');
+        $telefono = $post->request->get('telefono');
+        $email = $post->request->get('email');
+        $direccion = $post->request->get('direccion');
+        $especialidad = $post->request->get('especialidad');
+        $hospital = $post->request->get('hospital');
+
+        $retArray = array(
+          'nombres' => '',
+          'apellidos' => '',
+          'registro' => '',
+          'identificacion' => '',
+          'telefono' => '',
+          'email' => '',
+          'direccion' => '',
+          'especialidad' => '',
+          'hospital' => ''
+        );
+
+        if ($post->request->has('submit')) {
+          // Establecer conexión al servidor MySQL local
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          // Abortar si no se puede establecer conexión al servidor
+          if ($conn->connect_error) {
+              $retArray['nombres'] = $nombres;
+              $retArray['apellidos'] = $apellidos;
+              $retArray['registro'] = $registro;
+              $retArray['identificacion'] = $identificacion;
+              $retArray['telefono'] = $telefono;
+              $retArray['email'] = $email;
+              $retArray['direccion'] = $direccion;
+              $retArray['especialidad'] = $especialidad;
+              $retArray['hospital'] = $hospital;
+              die("Connection failed: " . $conn->connect_error);
+          }
+
+          $sql = 'INSERT INTO `tabla24` (`nombres`, `apellidos`, `registro`, `identificacion`, `telefono`, `email`, `direccion`, `especialidad`, `hospital`)';
+          $sql .= ' VALUES(';
+          $sql .= '"' . $nombres . '", ';
+          $sql .= '"' . $apellidos . '", ';
+          $sql .= '"' . $registro . '", ';
+          $sql .= '"' . $identificacion . '", ';
+          $sql .= '"' . $telefono . '", ';
+          $sql .= '"' . $email . '", ';
+          $sql .= '"' . $direccion . '", ';
+          $sql .= '"' . $especialidad . '", ';
+          $sql .= '"' . $hospital . '");';
+
+          // echo $sql;
+
+          if ($conn->query($sql) === TRUE) {
+              $retArray['resultado'] = "Datos insertados éxitosamente";
+          } else {
+              $retArray['nombres'] = $nombres;
+              $retArray['apellidos'] = $apellidos;
+              $retArray['registro'] = $registro;
+              $retArray['identificacion'] = $identificacion;
+              $retArray['telefono'] = $telefono;
+              $retArray['email'] = $email;
+              $retArray['direccion'] = $direccion;
+              $retArray['especialidad'] = $especialidad;
+              $retArray['hospital'] = $hospital;
+              $retArray['error'] = "Ocurrió un error al insertar los datos: " . $conn->error;
+          }
+        }
+
         return $this->render(
-            'Ingreso/ingreso.html.twig',
-            array(
-                'nombres' => '',
-                'apellidos' => '',
-                'registro' => '',
-                'identificacion' => '',
-                'telefono' => '',
-                'email' => '',
-                'direccion' => '',
-                'especialidad' => '',
-                'hospital' => '',
-                'nombre' => '',
-            )
+            'Ingreso/ingreso.html.twig', $retArray
         );
     }
 
